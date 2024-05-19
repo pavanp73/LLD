568c84c0655e128832da63f36da98721996342c4
pavanp73 <pavan.padmanabhi@gmail.com>
2024-05-19 15:16:41 +0530

    some more abstraction

--- src/main/java/designpatterns/adapter/PhonePe.java
+++ src/main/java/designpatterns/adapter/PhonePe.java
@@ -2,14 +2,13 @@ package designpatterns.adapter;
 
 public class PhonePe {
 
-    static BankAPI bankAPI = new ICICIAdapter();
-
     /*public void sendMoney(String from, String to, int amount) {
         bankAPI.sendMoney(from, to, amount);
     }*/
 
     public static void main(String[] args) {
+        final BankAPI bankAPI = BankAPIFactory.getBankAPIFactory(Bank.ICICIBANK);
+        assert bankAPI != null;
         bankAPI.sendMoney("1234", "6578", 1000);
     }
-
 }
--- src/main/java/designpatterns/adapter/thirdparty/yesbank/YesBank.java
+++ src/main/java/designpatterns/adapter/thirdparty/yesbank/YesBank.java
@@ -3,6 +3,6 @@ package designpatterns.adapter.thirdparty.yesbank;
 public class YesBank {
 
     public void transferMoney(int amount, String from, String to) {
-        System.out.println("Transferring money from Yes Bank");
+        System.out.println("Transferring money via Yes Bank");
     }
 }
