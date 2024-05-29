8ebf8e5dc7606c5ef7960e456d5dbb231da59f78
pavanp73 <pavan.padmanabhi@gmail.com>
2024-05-29 18:13:16 +0530

    col and diagonal winning strategy

--- src/main/java/machinecoding/tictactoe/strategies/winningstrategy/ColWinningStrategy.java
+++ src/main/java/machinecoding/tictactoe/strategies/winningstrategy/ColWinningStrategy.java
@@ -3,9 +3,28 @@ package machinecoding.tictactoe.strategies.winningstrategy;
 import machinecoding.tictactoe.models.Board;
 import machinecoding.tictactoe.models.Move;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ColWinningStrategy implements GameWinningStrategy {
+    private final Map<Integer, HashMap<Character, Integer>> colMaps = new HashMap<>();
+
     @Override
     public boolean checkWinner(Board board, Move move) {
-        return false;
+        int col = move.getCell().getCol();
+        Character currentChar = move.getPlayer().getSymbol().getcChar();
+
+        if (!colMaps.containsKey(col)) {
+            colMaps.put(col, new HashMap<>());
+        }
+
+        HashMap<Character, Integer> currColCharMap = colMaps.get(col);
+
+        if (!currColCharMap.containsKey(currentChar)) {
+            currColCharMap.put(currentChar, 0);
+        }
+        currColCharMap.put(currentChar, currColCharMap.get(currentChar) + 1);
+
+        return currColCharMap.get(currentChar).equals(board.getSize());
     }
 }
--- src/main/java/machinecoding/tictactoe/strategies/winningstrategy/DiagonalWinningStrategy.java
+++ src/main/java/machinecoding/tictactoe/strategies/winningstrategy/DiagonalWinningStrategy.java
@@ -3,9 +3,38 @@ package machinecoding.tictactoe.strategies.winningstrategy;
 import machinecoding.tictactoe.models.Board;
 import machinecoding.tictactoe.models.Move;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiagonalWinningStrategy implements GameWinningStrategy {
+
+    private final Map<Character, Integer> leftDiagonalMap = new HashMap<>();
+    private final Map<Character, Integer> rightDiagonalMap = new HashMap<>();
+
     @Override
     public boolean checkWinner(Board board, Move move) {
-        return false;
+        int row = move.getCell().getRow();
+        int col = move.getCell().getCol();
+        Character currentChar = move.getPlayer().getSymbol().getcChar();
+
+        if (row == col) {
+            if (!leftDiagonalMap.containsKey(currentChar)) {
+                leftDiagonalMap.put(currentChar, 0);
+            }
+            leftDiagonalMap.put(currentChar, leftDiagonalMap.get(currentChar));
+        }
+
+        if (row + col == board.getSize() - 1) {
+            if (!rightDiagonalMap.containsKey(currentChar)) {
+                rightDiagonalMap.put(currentChar, 0);
+            }
+            rightDiagonalMap.put(currentChar, rightDiagonalMap.get(currentChar));
+        }
+
+        return (row == col &&
+                leftDiagonalMap.get(currentChar).equals(board.getSize())) ||
+                (row + col == board.getSize() - 1 &&
+                        rightDiagonalMap.get(currentChar).equals(board.getSize()));
+
     }
 }
